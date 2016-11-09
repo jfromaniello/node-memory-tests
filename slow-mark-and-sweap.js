1888c5fd67ab553b64d9c4e4b5e87cb77a3d4e0e
José F. Romaniello <jfromaniello@gmail.com>
2016-11-09 15:28:48 -0300

    minor

--- slow-mark-and-sweap.js
+++ slow-mark-and-sweap.js
@@ -9,22 +9,20 @@ const gcType = new Map([
   [15, 'All']
 ]);
 
+const stuf = [];
+
 gcStats.on('stats', function (stats) {
   if (stats.pauseMS < 100) { return; }
   stats.type = gcType.get(stats.gctype);
   console.log('GC happened', stats);
+  if (stats.pauseMS > 300) { stuf.splice(0); }
 });
-
-const stuf = [];
 
 setInterval(() => {
   var start = Date.now();
-  while(Date.now() - start < 1000){
+  while(Date.now() - start < 1200){
     stuf.push(stuf);
     stuf.push({});
   }
   console.log('----iteration finished----');
 }, 5000);
-
-
-console.log(stuf.length);
